5615a30285e8d43a418d880e2a7311ef9650f2f7
lisong <songli1988@gmail.com>
2020-02-04 21:24:46 +0800

    init express demo

---
+++ node/hello-express/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.listen(port, function(){
+    console.log('Express app listening on port ' + port);
+});
+
+app.get('/', function(request, response){
+    response.send('Hello, World!');
+});
+app.get('/test', function(request, response){
+    response.send('Hello there');
+});

a1d33c09a13b5633a0d40ff7b97a4a48ada4bb87
lisong <songli1988@gmail.com>
2020-02-04 21:34:31 +0800

    init quotes-api

---
+++ node/quotes-api/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.listen(port, function(){
+    console.log('Express app listening on port ' + port);
+});
+
+app.get('/', function(request, response){
+    response.send('Get request received at ' + request.url);
+});
